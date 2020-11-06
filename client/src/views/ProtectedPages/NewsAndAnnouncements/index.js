2e24b0867b0ee6492c888d41d0512862a3fcb64f
obitrim <pevangelist516@gmail.com>
2020-11-06 20:53:34 +0000

    created news component

--- client/src/views/ProtectedPages/NewsAndAnnouncements/index.js
+++ client/src/views/ProtectedPages/NewsAndAnnouncements/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './NewsAndAnnouncements.css';
+import Container from '../../../components/Container'
 
 const Index = (props) => {
-  return (
-    <h1>News and Announcements</h1>
-  )
+	return (
+		<div className="News">
+			<Container>
+		    	News and Annoucements
+		 	</Container>
+		</div>
+	)
 }
 
 export default Index;
