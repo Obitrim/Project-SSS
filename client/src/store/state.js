33875504b8f581a64acb85392b09bffac8c2f54e
obitrim <pevangelist516@gmail.com>
2020-11-06 20:51:32 +0000

    added user's state

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -1,3 +1,12 @@
 export default {
-	sidebarOpen: false
+	sidebarOpen: false,
+	user: {
+		firstName: 'Paul',
+		lastName: 'Obitrim',
+		username: 'wkObitrim',
+		studentNo: '20535730',
+		phoneNo: '0544923771',
+		email: 'pevangelist516@gmail.com',
+		knustEmail: 'pevangelist516@gmail.com'
+	}
 }
