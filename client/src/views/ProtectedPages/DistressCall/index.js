3b3fc45ce4e4fc336e60a41351e06cdcf679f694
obitrim <pevangelist516@gmail.com>
2020-11-06 20:51:54 +0000

    created distress call view

--- client/src/views/ProtectedPages/DistressCall/index.js
+++ client/src/views/ProtectedPages/DistressCall/index.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './DistressCall.css';
+import swal from 'sweetalert';
+import Container from '../../../components/Container';
+import SettingsVoiceIcon from '@material-ui/icons/SettingsVoice';
 
 const Index = (props) => {
-  return (
-    <h1>Distress call</h1>
-  )
+	const [isRecording, setIsRecording] = useState(false);
+	const [distressMessage, setDistressMessage] = useState("");
+
+	function startRecording(){
+		// set recording state
+		setIsRecording(!isRecording);
+		// initiate recording procedure
+	}
+
+	function sendRecording(){
+		// end recording
+		setIsRecording(false);
+		// validate non-empty distress message
+		// If distress description was added, add message
+		// Get recording data and send to appropriate agency.
+		//send a success or failure alert
+		swal({
+			title: 'Signal sent',
+			text: 'Distress signal has been sent to the appropriate agency.',
+			icon: 'success'
+		});
+	}
+
+	function onChangeHandler(evt){
+		setDistressMessage(evt.target.value);
+	}
+
+	return (
+		<div className="DistressCall">
+			<Container>
+				<div className="DistressCall__ImgBox">
+					<img className="DistressCall__ImgBox__Img" src="/svgs/SOS.svg" alt="SOS" />
+				</div>
+				<div className="DistressCall__DetailsBox">
+					<p className="DistressCall__DetailsBox__Info">
+						Send us a signal. <br/> 
+						Would you care to tell us the details of your distress?
+					</p>
+					<textarea 
+						className="MsgBox" 
+						rows="7" 
+						onChange={onChangeHandler}
+						placeholder="Send us a message"
+						value={ distressMessage }></textarea>
+					{isRecording && <p className="RecordingMsg">Recording... Click the record button to cancel</p>}
+					<div className="ButtonBar">
+						<button type="button"
+							className={`DistressCall__ButtonBar__Btn ${isRecording && 'Recording'}`}  
+							onClick={ startRecording }>
+							<SettingsVoiceIcon className="DistressCall__SettingsVoiceIcon"/>
+						</button>
+						<button 
+							type="button"
+							className="DistressCall__ButtonBar__Btn SendBtn" 
+							onClick={sendRecording}>
+							send
+						</button>
+					</div>
+				</div>
+			</Container>
+		</div>
+	)
 }
 
 export default Index;
