cf52c77b7e77e943f6e8b6022cbbed6e2307b2eb
Richard Arthur <richardarthur228@gmail.com>
2020-11-07 21:25:35 +0000

    Added Hostel list view

--- client/src/views/ProtectedPages/Hostels/index.js
+++ client/src/views/ProtectedPages/Hostels/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React from "react";
+import "./Hostels.css";
+import Container from "../../../components/Container";
+import HostelCard from "../../../components/HostelCard";
 
 const Index = (props) => {
   return (
-    <h1>Hostel Finder</h1>
-  )
-}
+    <div className="HostelFinder">
+      <Container>
+        <div className="HostelFinder__Main">
+          <div className="HostelFinder__row">
+            <HostelCard
+              img={"/images/hostel_img.jpg"}
+              hostel_name={"Hostel Name"}
+              hostel_desc={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum."
+              }
+              hostel_rating={5}
+            />
+
+            <HostelCard
+              img={"/images/hostel_img.jpg"}
+              hostel_name={"Hostel Name"}
+              hostel_desc={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum."
+              }
+              hostel_rating={5}
+            />
+          </div>
+
+          <div className="HostelFinder__row">
+            <HostelCard
+              img={"/images/hostel_img.jpg"}
+              hostel_name={"Hostel Name"}
+              hostel_desc={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum."
+              }
+              hostel_rating={5}
+            />
+
+            <HostelCard
+              img={"/images/hostel_img.jpg"}
+              hostel_name={"Hostel Name"}
+              hostel_desc={
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum."
+              }
+              hostel_rating={5}
+            />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default Index;
