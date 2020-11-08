5ce3a1efae4194875cf0f5878a5e9eed833a5506
konlan <konlanmikpekoah.km@outlook.com>
2020-11-08 01:24:04 +0000

    this is the app shell for the backend

---
+++ server/models/Hostels.js
@@ -0,0 +1,28 @@
+"use strict"
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Hostel= new Schema({
+    Name: {
+        type: String
+    },
+    Location: {
+        type: String
+    },
+    Distance_Campus: {
+        type: String
+    },
+    Picture: {
+        type: Number
+    },
+    Category: {
+        type: String
+    },
+   Rating: {
+        type: String
+    },
+    Phone: {
+        type: Number
+    }
+});
+module.exports = mongoose.model("Hostels", Hostel);
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+"use strict"
+const express = require("express");
+const router = express.Router();
+
+
+router.post('/A', (req, res)=>{
+   
+});
+
+
+
+module.exports = router;
