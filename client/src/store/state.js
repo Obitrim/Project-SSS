63ce7813cb35602ac9c748bec93394a4cbb62042
obitrim <pevangelist516@gmail.com>
2020-11-09 19:10:19 +0000

    defined hostels data model

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -8,5 +8,69 @@ export default {
 		phoneNo: '0544923771',
 		email: 'pevangelist516@gmail.com',
 		knustEmail: 'pevangelist516@gmail.com'
-	}
+	},
+	
+	hostels: [
+		{
+			id: 1,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 5
+		},{
+			id: 2,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 4
+		},{
+			id: 3,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 3
+		},{
+			id: 4,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 4
+		},{
+			id: 5,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 5
+		},{
+			id: 6,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 3
+		},{
+			id: 7,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 3
+		},{
+			id: 8,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 4
+		},{
+			id: 9,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 5
+		},{
+			id: 10,
+			name: 'Hostel name',
+			img: '/images/hostel_img.jpg',
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem totam ipsam vitae veniam, nesciunt voluptate voluptatum.",
+			hostel_rating: 3
+		}
+	]
 }
