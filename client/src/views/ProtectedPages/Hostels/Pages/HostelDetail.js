d212e8a1a91b2c4dd91340009388cca514ffc302
Richard Arthur <richardarthur228@gmail.com>
2020-11-09 14:30:18 +0000

    refactored hostel

---
+++ client/src/views/ProtectedPages/Hostels/Pages/HostelDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HostelCard from '../../../../components/HostelCard/HostelCard'
+
+const Index = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Index
