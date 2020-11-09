3e0d77397b660d3812852a2db9e24ca5f5ba7038
obitrim <pevangelist516@gmail.com>
2020-11-09 19:11:20 +0000

    refactored template

--- client/src/views/ProtectedPages/Hostels/Pages/HostelDetail.js
+++ client/src/views/ProtectedPages/Hostels/Pages/HostelDetail.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import HostelCard from '../../../../components/HostelCard/HostelCard'
+import React, { useState, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { StoreContext } from '../../../../store';
+import HostelCard from '../../../../components/HostelCard';
 
-const Index = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Index = (props) => {
+	const { id } = useParams();
+	const { store } = useContext(StoreContext);
+	const detailsOfHostelOfInterest = store.hostels.find(hostel => hostel.id == id);
 
-export default Index
+	if(!detailsOfHostelOfInterest){
+		return null;
+	}
+
+	return <h1>{detailsOfHostelOfInterest.id}</h1>
+}	
+
+export default Index;
