97852d74a206c193a7f24c98555cad2ec43df5ee
obitrim <pevangelist516@gmail.com>
2020-11-10 12:28:44 +0000

    refactored template

--- client/src/views/ProtectedPages/WhistleBlow/index.js
+++ client/src/views/ProtectedPages/WhistleBlow/index.js
@@ -1,25 +1,25 @@
 import './WhistleBlowStyle.css';
 import React from 'react';
+import InfoIcon from '@material-ui/icons/Info';
 
 const Index = (props) => {
   return (
-    <div class="content">
-      <div class="whistle-head">
-        {/* <h1>Whistle Blow</h1> */}
-      </div>
-
+    <section class="WhistleBlow V-whitespace">
+      <h1 class="ViewHeading">Whistle Blow</h1> 
+      <p className="WhistleBlow__Info">
+        <InfoIcon className="InfoIcon"/>
+        Help a victim of abuse by making a report. Your identity will not be known
+      </p>
       <div class="descrip-body">
-        <span class="descrip-head" > Description </span>
+        <span class="descrip-head"> Report Description </span>
         <div class="input-section">
-          <textarea class="text-input" rows="12" cols="70" placeholder="Your message">
-
-          </textarea>
+          <textarea class="text-input" rows="10" cols="70" placeholder="What is happening ?"></textarea>
         </div>
         <span class="send-bttn">
           <input type="button" value="send" name="send"></input>
         </span>
       </div>
-    </div>
+    </section>
   )
 }
 
