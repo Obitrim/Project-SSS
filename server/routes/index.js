00b89a4b122c3cfa48124124a693fdf7154b4e96
konlan <konlanmikpekoah.km@outlook.com>
2020-11-11 02:06:56 +0000

    I add a oust route to add the financial assistance data

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,22 @@
 "use strict"
 const express = require("express");
 const router = express.Router();
-
+let finAsistance = require("../models/FinancialAs");
 
 router.post('/A', (req, res)=>{
    
 });
 
-
+router.post('/add-finacialassistance', (req, res)=>{
+    
+    let finAsistanceData = new finAsistance(req.body);
+    finAsistanceData .save()
+    .then(finAsistanceData =>{
+        res.status(200).json({'finAsistanceData ': 'Financial assistance Data  added successfully'})
+    })
+    .catch(err=>{
+        res.status(400).send('your request for financial assistance was unseccesful');
+    });
+});
 
 module.exports = router;
