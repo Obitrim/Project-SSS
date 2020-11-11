d5b97c55694b5423dc7c21c88b5037331e9595b8
Richard Arthur <richardarthur228@gmail.com>
2020-11-11 00:23:29 +0000

    Added Hostel Detail

--- client/src/components/HostelCard/index.js
+++ client/src/components/HostelCard/index.js
@@ -1,16 +1,35 @@
-import './HostelCard.css';
+import "./HostelCard.css";
 import React from "react";
-import { useHistory } from 'react-router-dom';
-import StarRateIcon from '@material-ui/icons/StarRate';
+import { useHistory } from "react-router-dom";
+import StarRateIcon from "@material-ui/icons/StarRate";
 
 const Index = (props) => {
   const history = useHistory();
 
   const hostelRating = [];
-  for (let i=0; i<props.hostel_rating; i++){
-      hostelRating.push(<StarRateIcon />)
+  for (let i = 0; i < props.hostel_rating; i++) {
+    hostelRating.push(<StarRateIcon />);
   }
-    
+
+  const buttons = () => {
+    return (
+      <>
+        <div>
+          <button
+            className="HostelCard__Button"
+            onClick={() => history.push(`/app/hostels/${props.id}`)}
+          >
+            {" "}
+            Preview
+          </button>
+        </div>
+        <div>
+          <button className="HostelCard__Button">Room Types</button>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="HostelCard">
       <div className="HostelCard__img">
@@ -18,26 +37,32 @@ const Index = (props) => {
       </div>
       <div className="HostelCard__info">
         <div className="HostelCard__info__header">
-          <h4 className="HostelCard__info__title">{props.hostel_name}</h4>
+          <h4 className="HostelCard__info__title">
+            {props.hostel_name ? props.hostel_name : props.room_type}
+          </h4>
           <div className="HostelCard__info__ratings">
-            {hostelRating}
+            {props.hostel_rating ? hostelRating : props.price}
           </div>
         </div>
         <div className="HostelCard__info__desc">
-          <p>
-            {props.hostel_desc}
-          </p>
+          <p>{props.hostel_desc}</p>
         </div>
+        {/* <div className="HostelCard__buttons">
+          {props.room_type ? null : buttons()}
+        </div> */}
         <div className="HostelCard__buttons">
-          <div>
-            <button className="HostelCard__Button" 
-              onClick={() => history.push(`/app/hostels/${props.id}`)}
-              > Preview
-            </button>
-          </div>
-          <div>
-            <button className="HostelCard__Button">Room Types</button>
-          </div>
+        <div>
+          <button
+            className="HostelCard__Button"
+            onClick={() => history.push(`/app/hostels/${props.id}`)}
+          >
+            {" "}
+            Preview
+          </button>
+        </div>
+        <div>
+          <button className="HostelCard__Button">Room Types</button>
+        </div>
         </div>
       </div>
     </div>
--- client/src/views/ProtectedPages/Hostels/Pages/HostelDetail.js
+++ client/src/views/ProtectedPages/Hostels/Pages/HostelDetail.js
@@ -1,17 +1,66 @@
-import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import { StoreContext } from '../../../../store';
+import "../Hostels.css";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { StoreContext } from "../../../../store";
+import Container from "../../../../components/Container";
+import HostelCard from "../../../../components/HostelCard";
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 const Index = (props) => {
-	const { id } = useParams();
-	const { store } = useContext(StoreContext);
-	const detailsOfHostelOfInterest = store.hostels.find(hostel => hostel.id === id);
+  const { id } = useParams();
+  const { store } = useContext(StoreContext);
+  const detailsOfHostelOfInterest = store.hostels.find(
+    (hostel) => hostel.id === id
+  );
 
-	if(!detailsOfHostelOfInterest){
-		return null;
-	}
+  if (!detailsOfHostelOfInterest) {
+    return null;
+  }
 
-	return <h1>{detailsOfHostelOfInterest.id}</h1>
-}	
+  return (
+    <div className="V-whitespace">
+      <Container>
+	  <h1 className="ViewHeading">Hostel Detail</h1>
+        <div className="HostelDetail">
+          <div className="HostelDetail__top">
+			<div className="HostelDetail__card">
+				<HostelCard
+				key={detailsOfHostelOfInterest.id}
+				id={detailsOfHostelOfInterest.id}
+				img={detailsOfHostelOfInterest.img}
+				hostel_name={detailsOfHostelOfInterest.name}
+				hostel_desc={detailsOfHostelOfInterest.desc}
+				hostel_rating={detailsOfHostelOfInterest.hostel_rating}
+				room_type
+				/>
+			</div>
+			<div className="HostelDetail__facilities">
+				<h2 className="HostelDetail__facilities__title ViewHeading">Facilities</h2>
+				<ul className="HostelDetail__facilities__list">
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 1</li>
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 2</li>
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 3</li>
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 4</li>
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 5</li>
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 6</li>
+					<li><FiberManualRecordIcon fontSize="small" /> Facility 7</li>
+				</ul>
+			</div>
+		  </div>
+		  <div className="HostelDetail__bottom">
+		  <h2 className="ViewHeading">Room Types</h2>
+			<div className="HostelFinder__Main">
+				<HostelCard room_type="1 in 1" price="GHS 5000" img={detailsOfHostelOfInterest.img}/>
+				<HostelCard room_type="1 in 1" price="GHS 5000" img={detailsOfHostelOfInterest.img}/>
+				<HostelCard room_type="1 in 1" price="GHS 5000" img={detailsOfHostelOfInterest.img}/>
+				<HostelCard room_type="1 in 1" price="GHS 5000" img={detailsOfHostelOfInterest.img}/>
+				<HostelCard room_type="1 in 1" price="GHS 5000" img={detailsOfHostelOfInterest.img}/>
+			</div>
+		  </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default Index;
