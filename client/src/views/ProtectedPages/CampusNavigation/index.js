0422d3b0244a09af7cf4ab25499ca34d7cddb198
Dokonah <38753393+Dokonah@users.noreply.github.com>
2020-11-19 13:09:17 +0000

    Add files via upload

--- client/src/views/ProtectedPages/CampusNavigation/index.js
+++ client/src/views/ProtectedPages/CampusNavigation/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import './CampusNavigation.css';
 
 const Index = (props) => {
   return (
-    <h1>Campus Navigation</h1>
+    <div>
+      <h1>Campus Navigation</h1>
+      <form class="search">
+
+          <div class="contents">
+            <fieldset>
+          <label class="content_label_from">From</label>
+          <input name="From" class="content_input" type="text"></input><br></br>
+          <label class="content_label_to" for="to">To</label>
+          <input name="To" class="content_input" type="text"></input>
+          </fieldset>
+          </div>
+
+      </form>
+      <div class="maparea">
+        
+      </div>
+    </div>
+    
+
   )
 }
 
