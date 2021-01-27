7a2cf5e8884b26b3284d50c7a55be8a5bb43e118
obitrim <pevangelist516@gmail.com>
2021-01-27 20:27:46 +0000

    implemented screen overlay on sidebar show

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -12,16 +12,19 @@ const Index = (props) => {
 	}, [location, dispatch]);
 
 	return (
-		<nav className={`AppNavbar ${store.sidebarOpen && 'Show'}`}>
-			<NavLink className="NavLink" to="/app" exact activeClassName="ActiveLink">Home</NavLink>
-			<NavLink className="NavLink" to="/app/hostels" activeClassName="ActiveLink">Hostels</NavLink>
-			<NavLink className="NavLink" to="/app/finance" activeClassName="ActiveLink">Financial Assistance</NavLink>
-			<NavLink className="NavLink" to="/app/navigation" activeClassName="ActiveLink">Campus Navigator</NavLink>
-			<NavLink className="NavLink" to="/app/whistle-blow" activeClassName="ActiveLink">Whistle Blow</NavLink>
-			<NavLink className="NavLink" to="/app/news" activeClassName="ActiveLink">News</NavLink>
-			<NavLink className="NavLink" to="/app/counselling" activeClassName="ActiveLink">Counselling</NavLink>
-			<NavLink className="NavLink" to="/app/distress" activeClassName="ActiveLink">SOS</NavLink>
-		</nav>
+		<>
+			<nav className={`AppNavbar ${store.sidebarOpen && 'Show'}`}>
+				<NavLink className="NavLink" to="/app" exact activeClassName="ActiveLink">Home</NavLink>
+				<NavLink className="NavLink" to="/app/hostels" activeClassName="ActiveLink">Hostels</NavLink>
+				<NavLink className="NavLink" to="/app/finance" activeClassName="ActiveLink">Financial Assistance</NavLink>
+				<NavLink className="NavLink" to="/app/navigation" activeClassName="ActiveLink">Campus Navigator</NavLink>
+				<NavLink className="NavLink" to="/app/whistle-blow" activeClassName="ActiveLink">Whistle Blow</NavLink>
+				<NavLink className="NavLink" to="/app/news" activeClassName="ActiveLink">News</NavLink>
+				<NavLink className="NavLink" to="/app/counselling" activeClassName="ActiveLink">Counselling</NavLink>
+				<NavLink className="NavLink" to="/app/distress" activeClassName="ActiveLink">SOS</NavLink>
+			</nav>
+			<div className={`AppNavbar__Overlay ${store.sidebarOpen && 'AppNavbar__Overlay--NavOpen'}`} onClick={() => dispatch({ type: 'SHOW_SIDEBAR', open: false})}></div>
+		</>
 	)
 }
 
