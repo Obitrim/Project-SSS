2856c4b2f35e341fea58796d323c4da82aa9ca2e
obitrim <pevangelist516@gmail.com>
2021-01-27 20:29:56 +0000

    refactored markup

--- client/src/views/Login/index.js
+++ client/src/views/Login/index.js
@@ -14,34 +14,37 @@ const Index = (props) => {
 			clearInterval(sliderInterval);
 		}
 	}, [activeIndex])
+	
 	return (
 		<section className="LoginView">
-			 <div className="LoginView__Row">
-		    	<div className="LoginView__Info">
-					<h1 className="LoginView__Info__Heading">KNUST Student Support Portal</h1>
-		    		<div className="SlidesWrapper" style={{ marginLeft: `${activeIndex * -100}%`}}>
-		    			<div className="Slide">
-		    				<img className="Slide__Icon" src="/svgs/campus_navigation_icon.svg" alt="campus navigation icon"/>
-		    				<img className="Slide__Icon" src="/svgs/hostel_finder_icon.svg" alt="hostel finder icon"/>
-		    				<p className="Slide__Text">Find your way around and book accomodations with ease</p>
-		    			</div>
-		    			<div className="Slide">
-		    				<img className="Slide__Icon" src="/svgs/financial_assistance_icon.svg" alt="financial assistance icon"/>
-		    				<img className="Slide__Icon" src="/svgs/counseling_assistance_icon.svg" alt="counseling assistance icon" />
-		    				<img className="Slide__Icon" src="/svgs/SOS_icon.svg" alt="sos icon" />
-		    				<p className="Slide__Text">Request Assistance</p>
-		    			</div>
-		    			<div className="Slide">
-		    				<img className="Slide__Icon" src="/svgs/news_announcement_icon.svg" alt="information dissipation" />
-		    				<p className="Slide__Text">Get informed and up to date</p>
-		    			</div>
-		    			<div className="Slide">
-		    				<img className="Slide__Icon" src="/svgs/whistle_blower_icon.svg" alt="whistle blower icon" />
-		    				<p className="Slide__Text">Be an Informant</p>
-		    			</div>
-		    		</div>
-		    	</div>
-		    	<LoginForm/>
+			<div className="LoginView__Layer">
+				 <div className="LoginView__Row">
+			    	<div className="LoginView__Info">
+						<h1 className="LoginView__Info__Heading">KNUST Student Support Portal</h1>
+			    		<div className="SlidesWrapper" style={{ marginLeft: `${activeIndex * -100}%`}}>
+			    			<div className="Slide">
+			    				<img className="Slide__Icon" src="/svgs/campus_navigation_icon.svg" alt="campus navigation icon"/>
+			    				<img className="Slide__Icon" src="/svgs/hostel_finder_icon.svg" alt="hostel finder icon"/>
+			    				<p className="Slide__Text">Find your way around and book accomodations with ease</p>
+			    			</div>
+			    			<div className="Slide">
+			    				<img className="Slide__Icon" src="/svgs/financial_assistance_icon.svg" alt="financial assistance icon"/>
+			    				<img className="Slide__Icon" src="/svgs/counseling_assistance_icon.svg" alt="counseling assistance icon" />
+			    				<img className="Slide__Icon" src="/svgs/SOS_icon.svg" alt="sos icon" />
+			    				<p className="Slide__Text">Request Assistance</p>
+			    			</div>
+			    			<div className="Slide">
+			    				<img className="Slide__Icon" src="/svgs/news_announcement_icon.svg" alt="information dissipation" />
+			    				<p className="Slide__Text">Get informed and up to date</p>
+			    			</div>
+			    			<div className="Slide">
+			    				<img className="Slide__Icon" src="/svgs/whistle_blower_icon.svg" alt="whistle blower icon" />
+			    				<p className="Slide__Text">Be an Informant</p>
+			    			</div>
+			    		</div>
+			    	</div>
+			    	<LoginForm/>
+			    </div>
 		    </div>
 		</section>
 	)
