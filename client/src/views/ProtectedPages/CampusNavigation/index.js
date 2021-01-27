d305ffd49737fe22c80d9a915cd79218c287cb48
obitrim <pevangelist516@gmail.com>
2021-01-27 20:28:11 +0000

    modified markup

--- client/src/views/ProtectedPages/CampusNavigation/index.js
+++ client/src/views/ProtectedPages/CampusNavigation/index.js
@@ -8,19 +8,7 @@ const Index = (props) => {
   return (
     <div className="CampusNavigator">
       <h1>Campus Navigation</h1>
-      <form class="search">
-
-          <div class="contents">
-            <fieldset>
-          <label class="content_label_from">From</label>
-          <input name="From" class="content_input" type="text"></input><br></br>
-          <label class="content_label_to" for="to">To</label>
-          <input name="To" class="content_input" type="text"></input>
-          </fieldset>
-          </div>
-
-      </form>
-      <Googlemap/>
+       <Googlemap/>
     </div>
   )
 }
