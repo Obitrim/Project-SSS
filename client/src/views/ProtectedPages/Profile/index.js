7c6d592bb13c2b1dc977541d6e17964d554a2e63
obitrim <pevangelist516@gmail.com>
2021-01-28 13:42:45 +0000

    modified icon name

--- client/src/views/ProtectedPages/Profile/index.js
+++ client/src/views/ProtectedPages/Profile/index.js
@@ -11,7 +11,7 @@ const Index = (props) => {
 
 	return (
 		<div className="ProfileView">
-			<img className="ProfileView__ProfileImg" src="/svgs/Big Profile.svg" alt="profile"/>
+			<img className="ProfileView__ProfileImg" src="/svgs/big_profile.svg" alt="profile"/>
 			<h2 className="ProfileView__Name">SURNAME, First Name</h2>
 			<form className="ProfileView__Form">
 				<input type="text" 
