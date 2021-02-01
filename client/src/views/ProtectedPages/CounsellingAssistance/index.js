ca57498c030d6cb6c49756c38af4c6ba4e261a5b
obitrim <pevangelist516@gmail.com>
2021-02-01 00:45:03 +0000

    implemented alert for form submission

--- client/src/views/ProtectedPages/CounsellingAssistance/index.js
+++ client/src/views/ProtectedPages/CounsellingAssistance/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import swal from 'sweetalert';
+
 import "./CounsellingAssistance.css";
 import StudentDetailsForm from '../../../components/StudentDetailsForm';
 
 const Index = (props) => {
   function onSubmit(formik){
-    alert('Submitting counselling details');
+    swal({
+      title: 'Success',
+      text: 'We are happy to receive your message for Financial assistance. You will hear from us soon',
+      icon: 'success'
+    }).then(() => {
+      formik.resetForm()
+    })
   }
 
   return (
