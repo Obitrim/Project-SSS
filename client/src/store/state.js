a2e42683edee1159e68a09ca22831750158129d2
obitrim <pevangelist516@gmail.com>
2021-02-03 17:24:30 +0000

    set initial user state to null

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -1,16 +1,6 @@
 export default {
 	sidebarOpen: false,
-	user: {
-		firstName: "Paul",
-		lastName: "Obitrim",
-		program: "BSc. Computer Science",
-		level: "400",
-		username: "pkobitrim",
-		studentNo: "20521730",
-		phoneNo: "0544923771",
-		email: "pevangelist516@gmail.com",
-		knustEmail: "pevangelist516@gmail.com"
-	},
+	user: null,
 	
 	hostels: [
 		{
