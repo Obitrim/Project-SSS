e8ade15d3d719e423b9403e4f6911a726d3b9e16
obitrim <pevangelist516@gmail.com>
2021-02-10 10:00:41 +0000

    added test data for local development

---
+++ server/test-data/hostels.js
@@ -0,0 +1,133 @@
+module.exports = [{
+	name: 'Wagyingo Hostel',
+	location: 'Ayeduase, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0549678089',
+	facilitites: [
+		'generator', 'self-contained rooms', 'shops', 
+		'parking space', 'TV room', 'wardrope', 'DSTV',
+		'Kitchenette', 'AC', 'Cleaning services', 'CCTV'
+	],
+	roomTypes: ['1', '2'],
+	bookingPortal: ''
+},{
+	name: 'Westend Hostel',
+	location: '',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0243881534',
+	facilitites: [
+		'Standby Generator', 'Security Post', 'TV room', 'Study room',
+		'Car park', '24hrs water', 'Gym', 'Air Conditioned rooms',
+		'Basketball court', 'shuttle', 'Disability-friendly',
+		'Kitchen', 'Pool', 'Bar', 'Gated', 'Wifi', 'Shops', 'TV'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Canam Hall Hostel',
+	location: 'Kotei, KNUST',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0244991398',
+	facilitites: ['study Area', 'water', 'Electricity', 'Kitchenette'],
+	roomTypes: ['1', '2', '3'],
+	bookingPortal: ''
+},{
+	name: 'Frontline Apartment Hostel',
+	location: 'Ayeduase, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 3,
+	phone: '0276907186',
+	facilitites: [
+		'generator', 'self-contained rooms', 'Parking Space',
+		'Hall for every Department', 'Kitchenette', 'CCTV', 'DSTV', 'Free Gas'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Frontline Inn Hostel',
+	location: 'Ayeduase, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0276907186',
+	facilitites: [
+		'generator', 'self-contained rooms', 
+		'parking space', 'TV room', 'DSTV',
+		'Kitchenette', 'AC', 'CCTV'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Frontline Court Hostel',
+	location: 'Kotei, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0276907186',
+	facilitites: [
+		'generator', 'self-contained rooms', 
+		'parking space', 'TV room', 'DSTV',
+		'Kitchenette', 'CCTV'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Rising Sun Hostel',
+	location: 'Ayeduase, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0244572880',
+	facilitites: [
+		'generator', 'self-contained rooms', 
+		'parking space', 'TV room', 'DSTV',
+		'Kitchenette', 'AC', 'CCTV'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Delisa Hostel',
+	location: 'Kotei, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0244282694',
+	facilitites: [
+		'generator', 'self-contained rooms', 
+		'parking space', 'TV room', 'DSTV',
+		'Kitchenette', 'AC', 'CCTV'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Adom Bi Hostel',
+	location: 'Ayeduase, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 4,
+	phone: '0244462154',
+	facilitites: ['Fridge', 'TV', 'AC'],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+},{
+	name: 'Happy Family Hostel',
+	location: 'Ayeduase, Kumasi',
+	distanceFromCampus: '',
+	category: 'Hostel',
+	rating: 5,
+	phone: '0243787505',
+	facilitites: [
+		'Generator', 'self-contained rooms', 
+		'parking space', 'TV room', 'DSTV',
+		'Kitchenette', 'Study Area'
+	],
+	roomTypes: ['2', '3', '4'],
+	bookingPortal: ''
+}];
---
+++ server/test-data/users.js
@@ -0,0 +1,36 @@
+module.exports = [
+	{
+		username: 'oaorison1',
+		password: '1234orison',
+		refNumber: '30125743',
+		firstName: 'Orison',
+		lastName: 'Ansre',
+		level: 200,
+		program: 'Petrochemical Engineering',
+		phone: '0544933371',
+		email: 'orison@gmail.com',
+		knustEmail: 'orison2@gmail.com'
+	}, {
+		username: 'Zion',
+		password: '1z2i3o4n',
+		refNumber: '2065817',
+		firstName: 'Zion',
+		lastName: 'Ignitious',
+		level: 300,
+		program: 'Natural Resource',
+		phone: '0544946371',
+		email: 'zion@gmail.com',
+		knustEmail: 'zion2@gmail.com'
+	},{
+		username: 'pkobitrim',
+		password: '4odstc34',
+		refNumber: '20521730',
+		firstName: 'Paul',
+		lastName: 'Obitrim',
+		level: 400,
+		program: 'BSc. Computer Science',
+		phone: '0544966371',
+		email: 'paulobitrim@gmail.com',
+		knustEmail: 'pevangelist516@gmail.com'
+	}
+];
