41efd6fd3f1af8efa1b2f6488b12253852bf7a4e
obitrim <pevangelist516@gmail.com>
2021-02-10 10:02:31 +0000

    implemented user navigation endpoints form

--- client/src/views/ProtectedPages/CampusNavigation/index.js
+++ client/src/views/ProtectedPages/CampusNavigation/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './CampusNavigation.css';
-
+import Paper from '@material-ui/core/Paper';
 import Googlemap from '../../../components/GoogleMap';
 
 const Index = (props) => {
 
   return (
     <div className="CampusNavigator">
-      <h1>Campus Navigation</h1>
+      <Paper className="CampusNavigator__EndPoints">
+      	<div className="FormGroup">
+      		<label className="FormGroup__Label" >Current Location</label>
+      		<input type="text" 
+      			className="EndPoints__Input"
+      			value="Kumasi, Ghana"
+      			name="currentLocation" 
+      			disabled
+      		/>
+      	</div>
+      	<div className="FormGroup">
+      		<label className="FormGroup__Label" htmlFor="destination">Destination</label>
+      		<input type="text" 
+      			className="EndPoints__Input EndPoints__Input--Destination"
+      			id="destination" 
+      			placeholder="where are you going?" 
+      			name="destination"
+      		/>
+      	</div>
+      </Paper>
        <Googlemap/>
     </div>
   )
